eba1213ba1116c025f862c272f555b06bb4c1d52
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-10-04 09:22:23 +0200

    Initial Commit

---
+++ journal_swiftui/journal_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  journal_swiftuiApp.swift
+//  journal_swiftui
+//
+//  Created by Saiman Chen on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct journal_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
